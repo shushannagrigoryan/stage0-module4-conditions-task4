3e968ad94ce584cb33b9b2135ba1aec2bf4bb44b
Shushanna <shushannagrigoryan7@gmail.com>
2022-08-18 16:14:18 +0400

    3

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -3,16 +3,25 @@ package school.mjc.stage0.conditions.task4;
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
         int max1;
-        if(first >= second){max1 = first;}
+        if(first >= second){
+            max1 = first;
+            if(max1>= third){
+                System.out.println(max1);
+            }
+            else{
+                System.out.println(third);
+            }
+        }
         else{
             max1 = second;
+            if(max1>= third){
+                System.out.println(max1);
+            }
+            else{
+                System.out.println(third);
+            }
         }
-        if(max1 >= third){
-            System.out.println(max1);
-        }
-        else{
-            System.out.println(third);
-        }
+
 
     }
 }
